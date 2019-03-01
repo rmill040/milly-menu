765b3135f3025339af76e6a4730d3d98eb8be8fd
Robert Milletich <R2@Roberts-Air.fios-router.home>
2019-03-01 01:09:39 -0500

    Minor update to install.sh

--- install.sh
+++ install.sh
@@ -7,20 +7,18 @@
 # General updates
 sudo yum update
 
-# Install git
-sudo yum install -y git
+# Install go
+sudo yum install -y go
 
 # Install MongoDB
 sudo mv mongodb-org-4.0.repo /etc/yum.repos.d/mongodb-org-4.0.repo
 sudo yum install -y mongodb-org
 
 # Clone milly-menu repo
-git clone https://github.com/rmill040/milly-menu.git
-cd milly-menu 
 go get -d ./...
 go build
 ./milly-menu --configure
-chmod 755 milly-menu run.sh
+chmod 755 run.sh
 
 # Define cronjob for every Saturday at 8:00AM
 # First write out current crontab
